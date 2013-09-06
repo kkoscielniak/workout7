3bcb6c9c7ca39b9a53aeac3a5a5ccb93f7e39d4c
Krystian Kościelniak <Krystian@chudy.(none)>
2013-09-06 19:18:00 +0200

    v 1.0.0.3 - bez reklam

---
+++ workout7/About.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Documents;
+using Microsoft.Phone.Tasks;
+
+namespace workout7
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("http://well.blogs.nytimes.com/2013/05/09/the-scientific-7-minute-workout/");
+            webBrowserTask.Show(); 
+        }
+    }
+}
