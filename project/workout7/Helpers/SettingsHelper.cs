216a74fb3768286e34098b598544aa945761e234
Krystian Kościelniak <kryszan1@gmail.com>
2013-12-11 23:01:07 +0100

    streak counter on tile
    + final cuts for certification
    + tidying project repo

---
+++ project/workout7/Helpers/SettingsHelper.cs
@@ -0,0 +1,80 @@
+﻿using System.IO.IsolatedStorage;
+
+namespace workout7.Helpers
+{
+    public static class SettingsHelper
+    {
+        public static bool IsFirstRun
+        {
+            get
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("isFirstRun"))
+                {
+                    return ((bool)IsolatedStorageSettings.ApplicationSettings["isFirstRun"]);
+                }
+                else
+                    return true;
+            }
+            set
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("isFirstRun"))
+                {
+                    IsolatedStorageSettings.ApplicationSettings["isFirstRun"] = value;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings.Add("isFirstRun", value);
+                }
+            }
+        }
+
+        public static bool StreakCounterEnabled
+        {
+            get
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("streakCounterEnabled"))
+                {
+                    return ((bool)IsolatedStorageSettings.ApplicationSettings["streakCounterEnabled"]);
+                }
+                else
+                    return true;
+            }
+            set
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("streakCounterEnabled"))
+                {
+                    IsolatedStorageSettings.ApplicationSettings["streakCounterEnabled"] = value;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings.Add("streakCounterEnabled", value);
+                }
+            }
+        }
+
+        public static int CurrentStreak
+        {
+            get
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("currentStreak"))
+                {
+                    return ((int)IsolatedStorageSettings.ApplicationSettings["currentStreak"]);
+                }
+                else
+                    return 0;
+            }
+            set
+            {
+                if (IsolatedStorageSettings.ApplicationSettings.Contains("currentStreak"))
+                {
+                    IsolatedStorageSettings.ApplicationSettings["currentStreak"] = value;
+                }
+                else
+                {
+                    IsolatedStorageSettings.ApplicationSettings.Add("currentStreak", value);
+                }
+            }
+        }        
+    }
+}
+
